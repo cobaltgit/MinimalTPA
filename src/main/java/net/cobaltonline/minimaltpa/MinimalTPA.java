1da399a4e322dae6705768b5c1be240b06100e26
cobaltgit <cobaltmail4204@gmail.com>
2022-11-13 13:06:19 +0000

    Remove unused imports for 1.1.1

--- src/main/java/net/cobaltonline/minimaltpa/MinimalTPA.java
+++ src/main/java/net/cobaltonline/minimaltpa/MinimalTPA.java
@@ -9,11 +9,6 @@ import net.cobaltonline.minimaltpa.listeners.PlayerListener;
 
 import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
