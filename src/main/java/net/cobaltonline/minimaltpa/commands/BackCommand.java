1fedca34082bd70cb5078eff0ad47b5ae001eb1b
cobaltgit <cobaltmail4204@gmail.com>
2022-11-13 12:33:52 +0000

    Add /back command

---
+++ src/main/java/net/cobaltonline/minimaltpa/commands/BackCommand.java
@@ -0,0 +1,46 @@
+package net.cobaltonline.minimaltpa.commands;
+
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.cobaltonline.minimaltpa.MinimalTPA;
+
+public class BackCommand implements CommandExecutor {
+    private MinimalTPA plugin;
+    public BackCommand(MinimalTPA plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage("This command cannot be used from the console.");
+            return true;
+        }
+
+        if (!this.plugin.backLocations.containsKey(player.getUniqueId())) {
+            player.sendMessage("You have no back locations!");
+            return true;
+        }
+
+        int cooldown = this.plugin.getConfig().getInt("cooldown");
+        if (this.plugin.backCooldowns.containsKey(player.getUniqueId()) && !player.hasPermission("minimaltpa.bypasscooldown")) {
+            long diff = (System.currentTimeMillis() - this.plugin.backCooldowns.get(player.getUniqueId())) / 1000;
+            if (diff < cooldown) {
+                player.sendMessage(String.format("Please wait %d seconds before going back to your previous location!", cooldown));
+                return true;
+            }
+        }
+
+        Location currentLocation = player.getLocation();
+        Location backLocation = this.plugin.backLocations.get(player.getUniqueId());
+        this.plugin.backLocations.put(player.getUniqueId(), currentLocation);
+        player.teleport(backLocation);
+        this.plugin.backCooldowns.put(player.getUniqueId(), System.currentTimeMillis());
+        player.sendMessage("Teleported to previous location!");
+        return true;
+    }
+}
